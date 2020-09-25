972d38ca07a6864ec821152ba1e03c6c53de406a
Jurica Bacurin <jurica@bacurin.de>
2020-09-25 20:59:47 +0200

    save progress

---
+++ frontend/recipeDB/IngredientModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace recipeDB
+{
+    public class IngredientModel
+    {
+        public string Name { get; set; }
+        public string Amount { get; set; }
+        public string Unit { get; set; }
+        public int SortOrder { get; set; }
+}
+}
---
+++ frontend/recipeDB/RecipeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace recipeDB
+{
+    public class RecipeModel
+    {
+        public Guid ID { get; set; }
+        public String Name { get; set; }
+        public List<IngredientModel> Ingredients { get; set; }
+        public List<StepModel> Steps { get; set; }
+    }
+
+    public class StepModel
+    {
+        public String Description { get; set; }
+    }
+}
