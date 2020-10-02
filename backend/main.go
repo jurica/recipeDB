7447d997840c6845cedc6090a2675046e5329115
Jurica Bacurin <jurica@bacurin.de>
2020-10-02 18:30:17 +0200

    - implemented basic CRUD operations  - removed old js/html frontend

--- backend/main.go
+++ backend/main.go
@@ -5,19 +5,17 @@ import (
 	"net/http"
 
 	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
-	scribble "github.com/nanobox-io/golang-scribble"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-var dbJSON, dbErr = scribble.New("./data", nil)
 var db *gorm.DB
 var err error
 
 func main() {
+	// TODO configure path to database file
 	db, err = gorm.Open(sqlite.Open("recipeDB.db"), &gorm.Config{})
 
 	if err != nil {
@@ -35,8 +33,6 @@ func main() {
 		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders: []string{"Content-Type"},
 	}))
-
-	r.Use(static.Serve("/", static.LocalFile("ui", true)))
 
 	// r.GET("/", index)
 	r.GET("/recipe", httpGetRecipes)
@@ -92,6 +88,7 @@ func httpPostRecipe(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
+		return
 	}
 
 	if recipe.ID == 0 {
@@ -100,12 +97,46 @@ func httpPostRecipe(c *gin.Context) {
 		})
 	}
 
-	// recipe, err = CreateOrUpdateRecipe(recipe)
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, gin.H{
-	// 		"error": err.Error(),
-	// 	})
-	// }
+	tx := db.Begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err = tx.Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err = tx.Delete(Ingredient{}, "recipe_id = ?", recipe.ID).Error; err != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err = tx.Delete(Step{}, "recipe_id = ?", recipe.ID).Error; err != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if err = tx.Save(&recipe).Error; err != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	tx.Commit()
 
 	c.JSON(http.StatusAccepted, recipe)
 }
@@ -121,24 +152,36 @@ func httpPutRecipe(c *gin.Context) {
 		})
 	}
 
-	// recipe, err = CreateOrUpdateRecipe(recipe)
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, gin.H{
-	// 		"error": err.Error(),
-	// 	})
-	// }
+	if recipe.ID != 0 {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "ID given for new recipe",
+		})
+	}
+
+	result := db.Create(&recipe)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+	}
 
 	c.JSON(http.StatusAccepted, recipe)
 }
 
 func httpDeleteRecipe(c *gin.Context) {
-	var err error
-
-	err = DeleteRecipe(c.Param("id"))
-
-	if err != nil {
+	recipe := Recipe{}
+	result := db.Find(&recipe, c.Param("id"))
+	if result.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	result = db.Delete(&recipe)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
 		})
 	}
 
--- backend/recipe.go
+++ backend/recipe.go
@@ -8,13 +8,13 @@ import (
 type Recipe struct {
 	gorm.Model
 	Name        string
-	Ingredients []Ingredient
-	Steps       []Step
+	Ingredients []Ingredient //`gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Steps       []Step       //`gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 // Ingredient single ingredient of a recipe
 type Ingredient struct {
-	gorm.Model
+	ID       uint
 	RecipeID uint
 	Name     string
 	Amount   string
@@ -23,35 +23,7 @@ type Ingredient struct {
 
 // Step contains the description text
 type Step struct {
-	gorm.Model
+	ID          uint
 	RecipeID    uint
 	Description string
-}
-
-// // NewRecipe get a new recipe with unique ID
-// func NewRecipe() Recipe {
-// 	recipe := Recipe{
-// 		ID: uuid.New(),
-// 	}
-
-// 	return recipe
-// }
-
-// CreateOrUpdateRecipe persists a recipe
-// func CreateOrUpdateRecipe(recipe Recipe) (newRecipe Recipe, err error) {
-// 	if recipe.ID == uuid.Nil {
-// 		recipe.ID = uuid.New()
-// 	}
-
-// 	// TODO couchDB like check for changes
-// 	if err := dbJSON.Write("recipe", recipe.ID.String(), recipe); err != nil {
-// 		return recipe, err
-// 	}
-
-// 	return recipe, nil
-// }
-
-// DeleteRecipe delete a recipe
-func DeleteRecipe(recipeID string) (err error) {
-	return dbJSON.Delete("recipe", recipeID)
 }
