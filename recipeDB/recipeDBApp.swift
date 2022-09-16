c5511f44aa5accf3c05e6d88045232be492a7f28
Jurica Bacurin <jurica@bacurin.de>
2022-09-16 18:12:26 +0200

    Initial Commit

---
+++ recipeDB/recipeDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeDBApp.swift
+//  recipeDB
+//
+//  Created by Jurica Bacurin on 16.09.22.
+//
+
+import SwiftUI
+
+@main
+struct recipeDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
